4e98636c05a6e21956ef39e67c9ca0f4385ed0c4
Jimmie Butler <jimmiebtlr@gmail.com>
2015-02-09 12:37:27 -0600

    Update accounts setup.

---
+++ client/accounts.js
@@ -0,0 +1,40 @@
+AccountsTemplates.configure({
+    // Behaviour
+    confirmPassword: true,
+    enablePasswordChange: true,
+    forbidClientAccountCreation: false,
+    overrideLoginErrors: true,
+    sendVerificationEmail: true,
+
+    // Appearance
+    showAddRemoveServices: false,
+    showForgotPasswordLink: true,
+    showLabels: true,
+    showPlaceholders: true,
+
+    // Client-side Validation
+    continuousValidation: false,
+    negativeFeedback: false,
+    negativeValidation: true,
+    positiveValidation: true,
+    positiveFeedback: true,
+    showValidating: true,
+
+    // Privacy Policy and Terms of Use
+    //privacyUrl: 'privacy',
+    //termsUrl: 'terms-of-use',
+
+    // Redirects
+    homeRoutePath: '/',
+    redirectTimeout: 4000,
+
+    // Hooks
+    //onLogoutHook: myLogoutFunc,
+    //onSubmitHook: mySubmitFunc,
+});
+
+AccountsTemplates.configureRoute('signIn');
+AccountsTemplates.configureRoute('signUp',{
+  path: "register"
+});
+
